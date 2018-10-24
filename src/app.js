268073edbc1e13e35ad23f9999a3d30b37228cd7
DeuterGraves <puckles@gmail.com>
2018-10-24 18:08:55 +0100

    country droplist now populated

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const CountriesData = require("./models/countries_data.js");
+const SelectView = require("./views/select_view.js");
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
 
   const countriesData = new CountriesData();
   countriesData.getData();
+
+  // select view bind events
+  const selectElement = document.querySelector("select#countries");
+  const countryDropdown = new SelectView(selectElement)
+  countryDropdown.bindEvents();
 });
--- src/views/select_view.js
+++ src/views/select_view.js
@@ -1,12 +1,28 @@
 const PubSub = require('../helpers/pub_sub.js');
 
 // select constructor
-const SelectView = function(){
+const SelectView = function(element){
   this.element = element;
-}
+};
 
 // binding events
-// subscribe - CountriesData:countries-loaded
+SelectView.prototype.bindEvents = function () {
+  // subscribe - CountriesData:countries-loaded
+  PubSub.subscribe("CountriesData:countries-loaded", (event) =>{
+    const allCountries = event.detail;
+    this.populate(allCountries);
+  });
+};
+
+// populate the drop down
+SelectView.prototype.populate = function (allCountries) {
+  allCountries.forEach((country, index) =>{
+    const option = document.createElement("option");
+    option.textContent = country.name;
+    option.value = index;
+    this.element.appendChild(option);
+  });
+};
 
 // listen for user selection
 // publish - country selected
